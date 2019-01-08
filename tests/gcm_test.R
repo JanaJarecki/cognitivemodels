c7a7fb03370cfcdc2648ab8ba91b172bda44b616
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-01-08 14:35:46 +0100

    Update gcm_test.R

--- tests/gcm_test.R
+++ tests/gcm_test.R
@@ -3,41 +3,42 @@ library(data.table)
 setwd("~/cogscimodels/tests")
 source("../models/gcm/gcm.R", chdir = TRUE)
 
-files <- list.files(path = "~/Masterarbeit/Experiment_Python/Categorization Experiment/data/pretest/csv/", pattern = "6", full.names = TRUE) # *.csv$
+files <- list.files(path = "~/Masterarbeit/Experiment_Python/Categorization Experiment/data/pretest/csv/", pattern = "", full.names = TRUE) # *.csv$
 dt <- rbindlist(lapply(files, fread, fill = TRUE))
-dt <- dt[block == "training", ]
 stim <- matrix(as.numeric(t(dt[, strsplit(as.character(stim), split = "")])), ncol = 3)
-
-# Fitting with solnp
-learn <- data.frame(stim, dt[, list(stim, true_cat)])
-colnames(learn) <- c(paste0("Dim", 1:3), "ID", "Cat")
-
-observed <- data.frame(stim, dt[, list(stim, response)])
-colnames(observed) <- c(paste0("Dim", 1:3), "ID", "Cat")
-
-N <- iterative.frequencies(x = learn)
-P <- make.prototype(x = learn, fun = cum.median.by.cat)
-P.ID <- simplify2array(lapply(P, rownames))
-
-result <- Max.likelihood.optimization.solnp(n.dim = 3,
-                                            Observedset = observed, 
-                                            Learningset = learn,
-                                            Model = "GCM", 
-                                            Metric = "Attr",
-                                            ignore = 0,
-                                            N = N,
-                                            P = P,
-                                            P.ID = P.ID)
-result$optimal.parameters
+dt <- cbind(stim, dt)
 
 # Fitting with gcm
-dt <- data.table(stim, dt[, list(true_cat, response)])
+fit_ll <- function(data, metric){
+  m <- gcm(formula = response ~ V1 + V2 + V3, data = data, cat = ~ true_cat, metric = metric, fixed = c(r = 1, p = 1), choicerule = "soft", discount = 8)
+  return(as.list(c(m$parm, loglik = m$logLik(), AIC = m$AIC())))
+}
 
-formula <- response ~ V1 + V2 + V3
+predict_gcm <- function(data, metric) {
+  m <- gcm(formula = response ~ V1 + V2 + V3, 
+           data = data[block == "training"], cat = ~ true_cat, metric = metric, 
+           fixed = c(r = 1, p = 1, w1 = data$w1[1], w2 = data$w2[1], w3 = data$w3[1], c = data$c[1], tau = data$tau[1]), 
+           choicerule = "soft")
+  c(m$predict(), m$predict(newdata = data[block == "test"]))
+}
 
-m <- gcm$new(formula, data = dt, cat = ~ true_cat, metric = "d", fixed = c(r = 1, p = 1))
-m$parm
-m$fit()
+# Fit Parameters of discrete and Minkowsi model
+fitted_parm_disc <- dt[block == "training", fit_ll(.SD, metric = "d"), by = subj_id]
+fwrite(fitted_parm_disc, file = "../data/Categorization/processed/fitted_parm_disc.csv")
+fitted_parm_mink <- dt[block == "training", fit_ll(.SD, metric = "m"), by = subj_id]
+fwrite(fitted_parm_mink, file = "../data/Categorization/processed/fitted_parm_mink.csv")
+setkey(fitted_parm_disc, subj_id)
+setkey(fitted_parm_mink, subj_id)
+
+# Predict for discrete and Minkowski model
+setkey(dt, subj_id)
+dt <- dt[fitted_parm_disc]
+dt[block != "familiarization", pred_disc := predict_gcm(data = .SD, metric = "d"), by = subj_id]
+dt[, colnames(fitted_parm_disc)[-1] := NULL]
+dt <- dt[fitted_parm_mink]
+dt[block != "familiarization", pred_mink := predict_gcm(data = .SD, metric = "m"), by = subj_id]
+dt[, colnames(fitted_parm_mink)[-1] := NULL]
+
 
 # dt <- data.table(c = c(0, 1, 1),
 #                  f1 = c(2, 1, 2),
