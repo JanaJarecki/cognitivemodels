92aa15b4cfb603acd0e903052e4526d0552598d5
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-01-29 15:16:06 +0100

    sets wd efficiently

--- tests/gcm_test.R
+++ tests/gcm_test.R
@@ -1,8 +1,9 @@
 rm(list=ls(all=T)) # Empty workspace
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("../models/gcm/gcm.R", chdir = TRUE)
 
 # Learning and test set
-dt <- data.table(response = c(0, 1, 1),
+dt <- data.table(response = c(0, 1, 0),
                  f1 = c(2, 1, 2),
                  f2 = c(1, 3, 2),
                  true_cat = c(1, 0, 1))
