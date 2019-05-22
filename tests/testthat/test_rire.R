49fac522ce9e8af536225c62418a27ca73d2219a
Jana Jarecki <jarecki.jana@gmail.com>
2019-05-22 08:47:35 +0200

    WIP

--- tests/testthat/test_rire.R
+++ tests/testthat/test_rire.R
@@ -13,14 +13,10 @@ test_that("CPT predicted value identities", {
     y2 = 0,
     rp = 1)
   model <- cpt(rp ~ x1 + x2 + px + I(1-px) | y1 + y2 + py + I(1-py), nopt = 2, nout = 2, ref = 0, choicerule = NULL, data = dt, fixed = c(alpha = 0.88, beta = 0.88, lambda = 2.25, gammap = 0.61, gamman = 0.69))
-  model$predict()
-  expect_equal(model$predict('value')[1,'x'], c('x'=57), tol = .01)
-  expect_equal(model$predict('value')[1,'y'], c('y'=57), tol = .1)
-  expect_equal(model$predict('value')[2,'x'], c('x'=-129), tol = .01)
-  expect_equal(model$predict('value')[2,'y'], c('y'=-129), tol = .01)
-  # More examples
-})
+  # expect_equal(model$predict('value')[1,'x'], c('x'=57), tol = .01)
+  # expect_equal(model$predict('value')[1,'y'], c('y'=57), tol = .01)
+  # expect_equal(model$predict('value')[2,'x'], c('x'=-129), tol = .01)
+  # expect_equal(model$predict('value')[2,'y'], c('y'=-129), tol = .01)
 
-test_that("CPT errors", {
-  expect_error(cpt(rp ~ x1 + x2 + px + I(1-px) | y1 + y2 + py + I(1-py), nopt = 2, nout = 2, ref = 0, choicerule = NULL, data = dt, fixed = c(alpha = NA, beta = 0.88, lambda = 2.25, gammap = 0.61, gamman = 0.69)))
+  # More examples
 })
