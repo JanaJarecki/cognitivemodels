4ab1f7652c3f1bdad048f3479d5a3345ba1a4932
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-10-29 15:05:16 +0100

    adds continuous data set

--- tests/gcm_test.R
+++ tests/gcm_test.R
@@ -4,22 +4,29 @@ library(R6)
 library(data.table)
 library(cogscimodels)
 library(devtools)
+library(Formula)
+library(Rsolnp)
 
 source("~/cogscimodels/R/cogscimodel-class.R", chdir = TRUE)
 source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
 source("~/cogscimodels/R/gcm.R", chdir = TRUE)
 
 # Stimuli
-dt <- data.table(response = c(0, 0, 1, 0, 1),
-                 f1 = c(4, 4, 1, 4, 1),
-                 f2 = c(1, 3, 1, 4, 4),
-                 true_cat = c(0, 0, 1, 0, 1))
+# dt <- data.table(response = c(0, 0, 1, 0, 1),
+#                  f1 = c(4, 4, 1, 4, 1),
+#                  f2 = c(1, 3, 1, 4, 4),
+#                  true_cat = c(0, 0, 1, 0, 1))
 
-dt <- data.table(response = c(0, 1, 0, 1, 1),
-                 f1 = c(4, 1, 3, 3, 2),
-                 f2 = c(1, 4, 2, 2, 4),
-                 true_cat = c(0, 1, 0, 0, 1))
+# dt <- data.table(response = c(0, 1, 0, 1, 1),
+#                  f1 = c(4, 1, 3, 3, 2),
+#                  f2 = c(1, 4, 2, 2, 3),
+#                  true_cat = c(0, 1, 0, 0, 1))
 
+n <- 20
+dt <- data.table(response = sample(c(0, 1), size = n, replace = TRUE),
+                 f1 = rnorm(n, mean = 2.5, sd = 0.5), 
+                 f2 = rnorm(n, mean = 2.5, sd = 0.5))
+dt[, true_cat := ifelse(abs(f1 - f2) < 0.5, 1, 0)]
 formula <- response ~ f1 + f2
 
 args <- list(formula = formula, cat = ~ true_cat, metric = "d", fixed = c(c = 1, r = 1, p = 1, tau = 1), choicerule = "soft")
