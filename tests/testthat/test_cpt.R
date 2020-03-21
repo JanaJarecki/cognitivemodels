5446e020c3f9196e7207be5512ef291d29eaa392
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-03-21 08:59:58 +0100

    makes predictive testing

--- tests/testthat/test_cpt.R
+++ tests/testthat/test_cpt.R
@@ -1,27 +1,52 @@
+# Tversky, A., & Kahneman, D. (1992). Advances in prospect theory: cumulative representation of uncertainty. Journal of Risk and Uncertainty, 5, 297–323. doi:10.1007/BF00122574
 context("cpt")
 library(cogsciMs)
 
-# Tversky, A., & Kahneman, D. (1992). Advances in prospect theory: cumulative representation of uncertainty. Journal of Risk and Uncertainty, 5, 297–323. doi:10.1007/BF00122574
-  # p. 313
-  dt <- data.frame(
-    x1 = c(100, -100),
-    px = 1,
-    x2 = 0,
-    y1 = c(200, -200),
-    py = c(.71,.64),
-    y2 = 0,
-    rp = 1)
-tk_par <- c(alpha = 0.88, beta = 0.88, lambda = 2.25, gammap = 0.61, gamman = 0.69)
-
+# 0. Data set, standard parameters, and tolerance
+dt <- data.frame(
+  x1 = c(100, -100),
+  px = 1,
+  x2 = 0,
+  y1 = c(200, -200),
+  py = c(.71, .64),
+  y2 = 0,
+  rp = 1) # Tversky & Kahneman, 1992, pp. 313
+tk_par <- c(alpha = 0.88, beta = 0.88, lambda = 2.25, gammap = 0.61, gamman = 0.69) 
+M <- cpt(rp ~ x1 + px + x2 | y1 + py + y2, ref = 0, data = dt, fix = tk_par)
 tol <- .01 
 
+# 1. Predictive testing
 test_that("Prediction identitites to Tversky & Kahneman (1992)", {
-  M <- cpt(rp ~ x1 + px + x2 | y1 + py + y2, ref = 0, data = dt, fix = tk_par)
   expect_equal(M$predict('value')[1,'pr_x'], c('pr_x'=57), tol = tol)
   expect_equal(M$predict('value')[1,'pr_y'], c('pr_y'=57), tol = tol)
   expect_equal(M$predict('value')[2,'pr_x'], c('pr_x'=-129), tol = tol)
   expect_equal(M$predict('value')[2,'pr_y'], c('pr_y'=-129), tol = tol)
 })
+
+# 1.a. Parameter value changes (lower, higher, boundaries)
+tk_par <- tk_par[sort(names(tk_par))]
+pars <- M$parspace[, 1:2]
+pars <- pars[sort(rownames(pars)), ]
+pars <- data.frame(pars, (pars + tk_par)/2)
+
+solutions <- array(data = c(4.65, 10000, 7.58, 758.58, rep(57.4, 16),
+                            2.86, 21616, 5.56, 1112.26, rep(57.23, 8), 105.9, 69.61, 21.01, 77.53, rep(57.23, 4),
+                            rep(-129.47, 4), -10.36, -22500, -17.07, -1706.8, rep(-129.47, 8), 0, -575.44, -64.74, -352.46,
+                            rep(-129.87, 4), -6.5, -49053.55, -12.62, -2524.03, 0, -132.92, -56.27, -154.08, rep(-129.87, 4), 0, -577.21, -64.94, -353.54), 
+                   dim = c(4, 5, 4), dimnames = list(c("lb", "ub", "lb.1", "ub.1"), names(tk_par), c("x1", "y1", "x2", "y2")))
+
+apply(as.matrix(expand.grid(1:length(tk_par), 1:ncol(pars))), 1, function(i) {
+  fix <- tk_par
+  fix[i[1]] <- pars[i[1], i[2]]
+  M <- cpt(rp ~ x1 + px + x2 | y1 + py + y2, ref = 0, data = dt, fix = fix)
+  test_that("Prediction identitites to Tversky & Kahneman (1992)", {
+    expect_equal(M$predict('value')[1,'pr_x'], c('pr_x' = solutions[i[2], i[1], 1]), tol = tol)
+    expect_equal(M$predict('value')[1,'pr_y'], c('pr_y' = solutions[i[2], i[1], 2]), tol = tol)
+    expect_equal(M$predict('value')[2,'pr_x'], c('pr_x' = solutions[i[2], i[1], 3]), tol = tol)
+    expect_equal(M$predict('value')[2,'pr_y'], c('pr_y' = solutions[i[2], i[1], 4]), tol = tol)
+  })
+})
+
 
 test_that("Prediction identities", {
   D <- data.frame(x1=c(1,2,2),x2=c(2,1,1),p=c(0.66,0.66,0.50))
