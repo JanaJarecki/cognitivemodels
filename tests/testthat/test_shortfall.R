099255769a8eb5e2f67e9c3a302d80559132f056
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-03-31 09:36:42 +0200

    makes parameter value changes

--- tests/testthat/test_shortfall.R
+++ tests/testthat/test_shortfall.R
@@ -21,6 +21,31 @@ test_that("Prediction identitites", {
   expect_equal(M$predict('value')[3,'pr_y'], c('pr_y'=2.5), tol = tol)
 })
 
+# 1.a. Parameter value changes (lower, higher, boundaries)
+pars <- M$parspace[, 1:2]
+pars <- pars[sort(rownames(pars)), ]
+pars <- data.frame(pars, (pars + par)/2)
+
+solutions <- array(data = c(rep(1.5, 8),
+                            1.5, -1, 0.875, -0.375, 1.5, -1, 0.875, -0.375,
+                            rep(1.5, 8),
+                            rep(2, 8), 
+                            rep(1.5, 8),
+                            rep(2.5, 8)), 
+                   dim = c(4, 2, 6), dimnames = list(c("lb", "ub", "lb.1", "ub.1"), names(par), c("x1", "y1", "x2", "y2", "x3", "y3")))
+
+apply(as.matrix(expand.grid(1:length(par), 1:ncol(pars))), 1, function(i) {
+  fix <- par
+  fix[i[1]] <- pars[i[1], i[2]]
+  M <- shortfall(choice ~ x1 + px + x2 + I(1-px) | y1 + py + y2 + I(1-py), data = dt, asp = ~aspiration, fix = fix)
+  test_that("Prediction identitites to Tversky & Kahneman (1992)", {
+    expect_equal(M$predict('value')[1,'pr_x'], c('pr_x' = solutions[i[2], i[1], 1]), tol = tol)
+    expect_equal(M$predict('value')[1,'pr_y'], c('pr_y' = solutions[i[2], i[1], 2]), tol = tol)
+    expect_equal(M$predict('value')[2,'pr_x'], c('pr_x' = solutions[i[2], i[1], 3]), tol = tol)
+    expect_equal(M$predict('value')[2,'pr_y'], c('pr_y' = solutions[i[2], i[1], 4]), tol = tol)
+  })
+})
+
 
 data(shortfalltest) # Load shortfall test data
 d2  <- shortfalltest[shortfalltest$id==2,]
