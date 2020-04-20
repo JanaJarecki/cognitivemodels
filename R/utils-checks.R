5df6e944a5545bf1d57c5b18a35fc57b8fe6a233
Jana Jarecki <jarecki.jana@gmail.com>
2020-04-21 01:13:07 +0200

    bugfix (overwritten checks by accidental deletion)

--- R/utils-checks.R
+++ R/utils-checks.R
@@ -19,26 +19,6 @@
   return(x)
 }
 
-
-
-#' Checks and optionally installs missing solvers
-#' 
-#' @param solver_name the name of the solver
-#' @export
-#' @noRd
-.match_and_check_solver <- function(solver) {
-  roi_solvers <- gsub("ROI.plugin.", "", ROI::ROI_available_solvers()$Package)
-  roi_registered <- names(ROI::ROI_registered_solvers())
-  roi_solvers <- unique(c(roi_solvers, roi_registered))
-  matched_solver <- try(match.arg(solver, c("grid", "solnp", "auto", roi_solvers), several.ok = TRUE), silent = TRUE)
-  if (inherits(matched_solver, "try-error")) {
-    stop("'solver' must be one of the following, rather than '", solver, "':\n    ", paste(sort(c("solnp", "auto", roi_solvers)), collapse = "\n    "))
-  } else {
-    solver <- unique(matched_solver)
-  }
-  if (length(solver) > 2L) {
-    stop("'solver' can have 2 entries, but it has ", length(solver), ".")
-=======
 #' Checks the parameter values
 #' 
 #' @param
@@ -46,7 +26,8 @@
 #' @noRd
 .check_par <- function(x = NULL, parspace, pass = FALSE) {
   # Formal checks
-  if (pass == TRUE || length(x) == 0L || fix[1] == "start") { return() }
+  if (pass == TRUE | length(x) == 0L) { return() }
+  if (is.character(x)) { if(x[1] == "start") { return() }}
   if (length(x) & all(is.numeric(x))) { x <- as.list(x) }
   if (length(x) > 1L & !is.list(x)) { 
     stop("Parameters to fix must be a list, not a ", typeof(x), ".\n  * Did you forget to supply a list to 'fix'? fix = list( ... )?", call.=FALSE)
@@ -140,8 +121,15 @@ solvers <- function() {
   if (length(solver) > 2L) {
     stop("'solver' must have 2 entries, not ", length(solver), ".")
   }
-  
-  missing <- is.na(match(solver, c("grid", "solnp", "auto", roi_registered)))
+  if (length(solver) == 2L) {
+    if (!any(grepl("grid", solver))) {
+      warning("Dropped the second solver '", solver[2], "', using only '", solver[1], "'.", call. = FALSE) 
+    } else if (solver[2] == "grid") {
+      solver <- solver[2:1]
+      warning("Using solver 'grid' first, then '", solver[2], "'.", call. = FALSE)
+    }
+  }
+  missing <- is.na(match(solver, c("grid", "solnp", "auto", names(ROI::ROI_registered_solvers()))))
   if (any(missing)) {
     install <- menu(c("Yes", "No, stop the model."), title = paste0("The solver '", solver[missing], "' is not (yet) installed. Want to install it?"))
     if (install == 1) {
