1ecbc32a0e3b4a98c9398a463e5d0e10b3d9dae8
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-06-30 10:02:25 +0200

    Create data-nosofsky1989long.R

---
+++ R/data-nosofsky1989long.R
@@ -0,0 +1,22 @@
+#' Test data for fitting the Exemplar-based categorization model
+#'
+#' Data about categorization in four different conditions.
+#' 
+#' \itemize{
+#'   \item condition. experimental condition (size, angle, criss, diag)
+#'   \item angle. feature value of the angle feature
+#'   \item size. feature value of the size feature
+#'   \item response. Which category has been selected? 
+#'   \item true_cat. True category label of this feature combination (0 or 1)
+#' }
+#' @name nosofsky1989long
+#'
+#' @docType data
+#' 
+#' @format An object of class \code{"data.frame"}.#'
+#' @keywords datasets
+#' @references Nosofsky, R. M. (1989). Further tests of an exemplar-similarity approach to relating identification and categorization. Perception & Psychophysics, 45, 279–290. doi:10.3758/BF03204942
+#' @source From Table 3 and Figure 2 in Nosofsky, R. M. (1989). Further tests of an exemplar-similarity approach to relating identification and categorization. Perception & Psychophysics, 45, 279–290. doi:10.3758/BF03204942
+#' @examples
+#' data(nosofsky1989long)
+NULL
