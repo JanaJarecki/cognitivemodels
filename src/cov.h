965620e17f138c4cab517d0aca009c3df4f6a42b
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-09-18 10:23:55 +0200

    makes header file for additional functions of MOD

---
+++ src/cov.h
@@ -0,0 +1,11 @@
+#ifndef __COV__
+#define __COV__
+
+float mean(Rcpp::NumericVector arr, int n);
+float covariance(Rcpp::NumericVector arr1, Rcpp::NumericVector arr2, int n);
+void getCofactor(Rcpp::NumericMatrix A, Rcpp::NumericMatrix temp, int p, int q, int n);
+float determinant(Rcpp::NumericMatrix A, int n);
+void adjoint(Rcpp::NumericMatrix A, Rcpp::NumericMatrix adj);
+Rcpp::NumericMatrix inverse(Rcpp::NumericMatrix A);
+
+#endif // __COV__
