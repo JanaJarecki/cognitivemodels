75a48381985e6e2e0128e335004214a239b63bda
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2021-01-27 20:19:02 +0100

    Update ebm.cpp

--- src/ebm.cpp
+++ src/ebm.cpp
@@ -161,12 +161,13 @@ Rcpp::NumericVector ebm_cpp(
       }
     }
 
-    // sim_all[t] = std::max(sim_all[t], DBL_EPSILON); // ensure sim[t] > 0    
-    if(sim_all[t] < DBL_EPSILON) {
+    if (sim_all[t] < DBL_EPSILON) {
       res[i] = R_NaN;
     } else{
       res[i] = val[t] / sim_all[t];
     }
+    // sim_all[t] = std::max(sim_all[t], DBL_EPSILON); // ensure sim[t] > 0
+    // res[i] = val[t] / sim_all[t];
     i += 1;
   }
 
